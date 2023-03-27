46e29ed0dca585db9b63d7671cb86c631fcd2c43
joshuandwilliams <joshuandwilliams@outlook.com>
2023-03-27 13:02:31 +0100

    both tif and TIF

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="cdascorer",
-    version='0.0.0.dev10',
+    version='0.0.0.dev11',
     packages = ['cdascorer', 'cdascorer-data'],
     url="https://github.com/joshuandwilliams/CDAScorer",
     license='LICENSE.txt',
